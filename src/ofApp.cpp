d270a753b27c02e634591b166682689f3e9c3622
Sam Ludford <sam@Sams-MacBook-Pro.local>
2018-01-05 20:40:55 +0000

    cleaned up code

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -17,17 +17,7 @@ int mode = 1;
 void ofApp::setup(){
     
     
-//    samp.load(ofToDataPath("chen_loop_4.wav"));
-    s1.load(ofToDataPath("s_1.wav"));
-    s2.load(ofToDataPath("s_2.wav"));
-    s3.load(ofToDataPath("s_3.wav"));
-    s4.load(ofToDataPath("s_4.wav"));
-    s5.load(ofToDataPath("s_5.wav"));
-    s6.load(ofToDataPath("s_6.wav"));
-    s7.load(ofToDataPath("s_7.wav"));
-    s8.load(ofToDataPath("s_8.wav"));
-    s9.load(ofToDataPath("s_9.wav"));
-    s10.load(ofToDataPath("dub_2.wav"));
+    samp.load(ofToDataPath("os_loop_1.wav"));
     
     ofBackground(230);
     ofSetColor(40);
@@ -48,7 +38,7 @@ void ofApp::setup(){
     ofEnableAlphaBlending();
     ofEnableSmoothing();
     
-    ofSoundStreamSetup(2,2,this, sampleRate, bufferSize, 4); /* this has to happen at the end of setup - it switches on the DAC */
+    ofSoundStreamSetup(2,2,this, sampleRate, bufferSize, 4);
     
     fft.setup(1024, 512, 256);
     oct.setup(44100, 1024, 7);
@@ -56,18 +46,6 @@ void ofApp::setup(){
     ofSetSphereResolution(3);
     
     // GUI
-    
-    params.add(s1_speed.set("s1 speed", 1.0, 0.01, 2));
-    params.add(s2_speed.set("s2 speed", 1.0, 0.01, 2));
-    params.add(s3_speed.set("s3 speed", 1.0, 0.01, 2));
-    params.add(s4_speed.set("s4 speed", 1.0, 0.01, 2));
-    params.add(s5_speed.set("s5 speed", 1.0, 0.01, 2));
-    params.add(s6_speed.set("s6 speed", 1.0, 0.01, 2));
-    params.add(s7_speed.set("s7 speed", 1.0, 0.01, 2));
-    params.add(s8_speed.set("s8 speed", 1.0, 0.01, 2));
-    params.add(s9_speed.set("s9 speed", 1.0, 0.01, 2));
-    params.add(s10_speed.set("s10 speed", 0.7, 0.01, 2));
-    
     gui.setup(params);
     
 }
@@ -80,15 +58,9 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    
-    if(saving) {
-        ofBeginSaveScreenAsPDF(ofToString(ofGetFrameNum()) + ".pdf");
-    }
-    
     if (mode == 1) {
         ofPushMatrix();
         ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
-//        ofRotate(ofGetFrameNum());
         
         
         float scale = 100.0;
@@ -100,12 +72,9 @@ void ofApp::draw(){
             float val = display_buffer[i];
             float rad = 0.0;
             float theta = 0.0;
-//            line.curveTo(sin(0.0) * rad * scale, cos(0.0) * rad * scale);
             while(theta <= 360.0) {
                 
                 float r = ofDegToRad(theta);
-                
-//                rad = sf(theta, oct.averages[0] * val * 10, oct.averages[1],oct.averages[2] * val,oct.averages[3],oct.averages[4], oct.averages[5] * val, oct.averages[6] * val);
                 
                 rad = sf(r, oct.averages[0] * val * 10, oct.averages[1],oct.averages[2] * val,oct.averages[3],oct.averages[4], oct.averages[5] * val, oct.averages[6] * val);
                 
@@ -121,20 +90,9 @@ void ofApp::draw(){
             line.draw();
         }
         
-//        line.curveTo(0.0, 0.0);
-//
-//        line.close();
-        
         ofPopMatrix();
     }
-
-
-    if(saving) {
-        ofEndSaveScreenAsPDF();
-        saving = false;
-    }
     
-//    gui.draw();
 }
 
 //--------------------------------------------------------------
@@ -142,24 +100,7 @@ void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
     
     for (int i = 0; i < bufferSize; i++){
         
-//        double speed =  ofMap(mouseX, 0, ofGetWidth(), 0.1, 1);
-//        double mix = samp.play(speed );
-//        double m1 = s1.play(s1_speed);
-//        double m2 = s2.play(s2_speed);
-//        double m3 = s3.play(s3_speed);
-//        double m4 = s4.play(s4_speed);
-//        double m5 = s5.play(s5_speed);
-//        double m6 = s6.play(s6_speed);
-//        double m7 = s7.play(s7_speed);
-//        double m8 = s8.play(s8_speed);
-//        double m9 = s9.play(s9_speed);
-        double m10 = s10.play(s10_speed);
-        
-//        double mix = (m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10);
-        
-//        double mix = m9 * 1.4;
-        
-        double mix = m10;
+        double mix = samp.play(speed);
         
         if (fft.process(mix)) {
             oct.calculate(fft.magnitudes);
@@ -188,18 +129,6 @@ void ofApp::audioIn(float * input, int bufferSize, int nChannels){
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     
-    if(key == ' ') {
-        saving = true;
-    } else if(key == '1') {
-        mode = 1;
-    } else if(key == '2') {
-        mode = 2;
-//        ofBackground(30);
-    } else if(key == '3') {
-        mode = 3;
-    } else if (key == '4') {
-        mode = 4;
-    }
 }
 
 //--------------------------------------------------------------
@@ -209,8 +138,7 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
-    
-    
+    speed = ofMap(x, 0, ofGetWidth(), -2, 2);
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -22,41 +22,30 @@ class ofApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
 
     
-    // Audio output and input methods
+        // Audio output and input methods
     
         void audioOut(float * output, int bufferSize, int nChannels);
         void audioIn(float * input, int bufferSize, int nChannels);
     
-    float sf(float angle, float a, float b, float m1, float m2, float n1, float n2, float n3);
+        float sf(float angle, float a, float b, float m1, float m2, float n1, float n2, float n3);
      
      
     
-    int		bufferSize; /* buffer size */
-    int		sampleRate;
+        int		bufferSize;
+        int		sampleRate;
     
-    ofxPanel gui;
+        ofxPanel gui;
     
-    ofParameterGroup params;
+        ofParameterGroup params;
+
+        maxiSample samp;
     
-    ofParameter<double> s1_speed;
-    ofParameter<double> s2_speed;
-    ofParameter<double> s3_speed;
-    ofParameter<double> s4_speed;
-    ofParameter<double> s5_speed;
-    ofParameter<double> s6_speed;
-    ofParameter<double> s7_speed;
-    ofParameter<double> s8_speed;
-    ofParameter<double> s9_speed;
-    ofParameter<double> s10_speed;
+        float speed {1.0};
     
+        maxiSample s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
     
-    
-    maxiSample samp;
-    
-    maxiSample s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
-    
-    ofxMaxiFFT fft;
-    ofxMaxiFFTOctaveAnalyzer oct;
+        ofxMaxiFFT fft;
+        ofxMaxiFFTOctaveAnalyzer oct;
     
 
 };
